900fc65fdef14eddde77e91215ce752416ed0b84
teddyamby <70837735+teddyamby@users.noreply.github.com>
2026-02-12 22:43:29 +0100

    Add Course model and update User model relationships

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Date, Float
+from sqlalchemy import Column, Integer, String, Date, Float, ForeignKey, Text
+from sqlalchemy.orm import relationship
 from .database import Base
 
 class User(Base):
@@ -11,5 +12,23 @@ class User(Base):
     birth_date = Column(Date)
     password_hash = Column(String)
 
-    study_time = Column(Float, default=0)
+    study_time = Column(Float, default=0.0)
     trees_financed = Column(Integer, default=0)
+    co2_consumed = Column(Float, default=0.0)
+
+    courses = relationship("Course", back_populates="user")
+
+
+class Course(Base):
+    __tablename__ = "courses"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    level = Column(String)
+    duration = Column(String)
+    content = Column(Text)
+
+    co2_generated = Column(Float)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="courses")
