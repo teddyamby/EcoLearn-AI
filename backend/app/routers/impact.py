3220eb46afec9c5089b8ab63de7150987da7dbcf
teddyamby <70837735+teddyamby@users.noreply.github.com>
2026-02-12 20:20:45 +0100

    Fix template response call in impact router

--- backend/app/routers/impact.py
+++ backend/app/routers/impact.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Request
+from ..templates_engine import templates
 
 router = APIRouter()
 
 @router.get("/impact")
 def impact(request: Request):
-    return request.app.templates.TemplateResponse(
-        "impact.html", {"request": request}
+    return templates.TemplateResponse(
+        "impact.html",
+        {"request": request}
     )
